f7606a2285ce8e9b194a825e830bb62d2e1127c1
Gabriel Magalhaes <digitalrx9@gmail.com>
2026-06-27 17:53:10 -0300

    fix: ajustado o ip do localhost

--- FrontEnd/Bugiganga/src/config/api.ts
+++ FrontEnd/Bugiganga/src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:8765';
+  process.env.EXPO_PUBLIC_API_URL ?? 'http://127.0.0.1:8765';
 
 export const API_ENDPOINTS = {
   auth: {
